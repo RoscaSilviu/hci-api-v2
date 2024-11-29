477ea2dc3ca15a918fc78111c6150f9d6f127384
Rosca Silviu <silviu.rosca@student.unitbv.ro>
2024-11-29 09:46:50 +0200

    test2

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using Azure.Identity;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-
+//Asd
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "CORS",
